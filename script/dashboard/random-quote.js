f7da7e6f33424955f08e9a99e6cbcbce9938fe47
King Pacaya <83373501+King-Pacaya@users.noreply.github.com>
2022-05-17 13:07:19 -0500

    Add files via upload

---
+++ script/dashboard/random-quote.js
@@ -0,0 +1,552 @@
+var quoteArray = [
+{
+content: "Mickey mouse is dead",
+author: "Me"
+},
+{
+content: "Chesse Burger Family",
+author: "Me"
+},
+{
+content: "Omg an onion!",
+author: "Me"
+},
+{
+content: "Omg an avocado!",
+author: "Me"
+},
+{
+content: "Everybody dance now 🎶",
+author: "Me"
+},
+{
+content: "Call me fighter",
+author: "Me"
+},
+{
+content: "Yes sir is a frog",
+author: "Me"
+},
+{
+content: "Yaw",
+author: "Me"
+},
+{
+content: "This is useful?",
+author: "Me"
+},
+{
+content: "Yes, today is the day",
+author: "Me"
+},
+{
+content: "Don't worry, everything will be okey",
+author: "Me"
+},
+{
+content: "Bibia be ye ye",
+author: "Me"
+},
+{
+content: "Idk, but you look beautiful today",
+author: "Me"
+},
+{
+content: "Stop, u need zzzzzzzzzleep",
+author: "Me"
+},
+{
+content: "Refresh the page",
+author: "Me"
+},
+{
+content: "XD",
+author: "Me"
+},
+{
+content: "Nothing here for now",
+author: "Me"
+},
+{
+content: "Remember drink water",
+author: "Me"
+},
+{
+content: "Just call me",
+author: "Me"
+},
+{
+content: "How many quotes are here?",
+author: "Me"
+},
+{
+content: "Hi :3",
+author: "Me"
+},
+{
+content: "I just wanna be a potato",
+author: "Me"
+},
+{
+content: "Nvm what r u doing but it's okay",
+author: "Me"
+},
+{
+content: "It's a combo sized meal, it's a family deal",
+author: "Me"
+},
+{
+content: "Keyboardmouse?",
+author: "Me"
+},
+{
+content: "Use ur glasses :D",
+author: "Me"
+},
+{
+content: "You'll get older, and maybe then you'll feel some control",
+author: "Me"
+},
+{
+content: "I can be brown",
+author: "Me"
+},
+{
+content: "I can be blue",
+author: "Me"
+},
+{
+content: "I can be violet sky",
+author: "Me"
+},
+{
+content: "Tomorrow is another day :D",
+author: "Me"
+},
+{
+content: "Blue!",
+author: "Me"
+},
+{
+content: "Yellow!",
+author: "Me"
+},
+{
+content: "Hold my gaze tonight",
+author: "Me"
+},
+{
+content: "The good thing ",
+author: "Me"
+},
+{
+content: "The best of the day is see you smile so bright",
+author: "Me"
+},
+{
+content: "Medicine at midnight",
+author: "Me"
+},
+{
+content: "Be chill",
+author: "Me"
+},
+{
+content: "Chillax",
+author: "Me"
+},
+{
+content: "Just another random quote",
+author: "Me"
+},
+{
+content: "Study",
+author: "Me"
+},
+{
+content: "Do you forget something?",
+author: "Me"
+},
+{
+content: "R u sure?",
+author: "Me"
+},
+{
+content: "Ya, that's okay",
+author: "Me"
+},
+{
+content: "No",
+author: "Me"
+},
+{
+content: "Yes",
+author: "Me"
+},
+{
+content: "U can't use me for your decisions",
+author: "Me"
+},
+{
+content: "Shut up, take a flower 🌹",
+author: "Me"
+},
+{
+content:"Is today your birthday?",
+author: "Me"
+},
+{
+content:"My rule's mark is in 18 cm",
+author: "Me"
+},
+{
+content:"Sorry i'm not okay for now",
+author: "Me"
+},
+{
+content:"Take a coffe and let's go ☕️",
+author: "Me"
+},
+{
+content:"Yes, u can be blue somedays",
+author: "Me"
+},
+{
+content:"After all, you have friends, family, and me, your dashboard :D",
+author: "Me"
+},
+{
+content:"Shine motherf*cker, shine a lot",
+author: "Me"
+},
+{
+content:"Turn off your PC",
+author: "Me"
+},
+{
+content:"No please, today nooooou",
+author: "Me"
+},
+{
+content:"I'm not superstitious, but i'm a little bit stitious",
+author: "Me"
+},
+{
+content:"Wassamassaw",
+author: "Me"
+},
+{
+content: "The planet is fine. The people are fucked",
+author: "George Carlin"
+},
+{
+content: "Stay up and fight",
+author: "Me"
+},
+{
+content: "Once you can accept the universe as matter expanding into nothing that is something, wearing stripes with plaid comes easy.",
+author: "Einstein"
+},
+{
+content: "I'm funny too :c",
+author: "Me"
+},
+{
+content: "What time is it?",
+author: "Me"
+},
+{
+content: "(It's) It's okey but (Is't), Is't okay?",
+author: "Me"
+},
+{
+content: "Close your phone",
+author: "Me"
+},
+{
+content: "Turn on you windows",
+author: "Me"
+},
+{
+content: "The worst religion is....",
+author: "Me"
+},
+{
+content: "01011000 01000100",
+author: "Me"
+},
+{
+content: "I can order to you a coffee or two",
+author: "Me"
+},
+{
+content: "Wala",
+author: "Me"
+},
+{
+content: "I write this at 5 a.m.",
+author: "Me"
+},
+{
+content: "Thanks for use me",
+author: "Me"
+},
+{
+content: "The yellow way",
+author: "Me"
+},
+{
+content: "I'd stay here forever looking in your eyes",
+author: "Me"
+},
+{
+content: "24/7, baby, 3-6-5",
+author: "Me"
+},
+{
+content: "The life's just a blink so don't think twice",
+author: "Me"
+},
+{
+content: "We could all be dead tomorrow :D",
+author: "Me"
+},
+{
+content: "It's not the end, 'cause the energy never dies",
+author: "Me"
+},
+{
+content: "You're amazing",
+author: "Me"
+},
+{
+content: "Tomorrow Is a long time away",
+author: "Me"
+},
+{
+content: "This night can last forever",
+author: "Me"
+},
+{
+content: "Someone like me should know better",
+author: "Me"
+},
+{
+content: "This night Is mine, It's only you and I",
+author: "Me"
+},
+{
+content: "Learn rusian",
+author: "Me"
+},
+{
+content: "Never let your best friends get lonely, keep disturbing them.",
+author: "Me"
+},
+{
+content: "Sometimes I wish I was an octopus, so I could slap eight people at once.",
+author: "Me"
+},
+{
+content: "If you’re hotter than me, then that means I’m cooler than you.",
+author: "Me"
+},
+{
+content: "My wallet is like an onion, opening it makes me cry.",
+author: "Me"
+},
+{
+content: "My goal this weekend is to move, just enough so people don’t think I’m dead.",
+author: "Me"
+},
+{
+content: "Lazy people fact #234782730901. You were too lazy to read that number.",
+author: "Me"
+},
+{
+content: "Friends buy you food. Best friends eat your food.",
+author: "Me"
+},
+{
+content: "Papercut: A tree’s final moment of revenge.",
+author: "Me"
+},
+{
+content: "Common sense is like deodorant, those who need it the most never use it.",
+author: "Me"
+},
+{
+content: "I don’t need a hair stylist, my pillow gives me a new hairstyle every morning.",
+author: "Me"
+},
+{
+content: "Life always offers you a second chance. It’s called tomorrow.",
+author: "Me"
+},
+{
+content: "My six pack is protected by a layer of fat.",
+author: "Me"
+},
+{
+content: "When nothing is going right, go left.",
+author: "Me"
+},
+{
+content: "If you have crazy friends you have everything you’ll ever need.",
+author: "Me"
+},
+{
+content: "Silence is golden, unless you have kids, then silence is just plain suspicious.",
+author: "Me"
+},
+{
+content: "I’m not running away from hard work, I’m too lazy to run.",
+author: "Me"
+},
+{
+content: "Don’t make me laugh, I’m trying to be mad at you.",
+author: "Me"
+},
+{
+content: "If I won the award for laziness, I would send somebody to pick it up for me.",
+author: "Me"
+},
+{
+content: "Maybe if we tell people the brain is an app, they’ll start using it.",
+author: "Me"
+},
+{
+content: "My bed is a magical place where I suddenly remember everything I forgot to do.",
+author: "Me"
+},
+{
+content: "A best friend is like a four-leaf clover, hard to find, lucky to have.",
+author: "Me"
+},
+{
+content: "Some people are like clouds. When they go away, it’s a brighter day.",
+author: "Me"
+},
+{
+content: "At night, I can’t fall asleep. In the morning, I can’t get up.",
+author: "Me"
+},
+{
+content: "Seeing a spider in my room isn’t scary. It’s scary when it disappears.",
+author:"Me"
+},
+{
+content: "If we shouldn’t eat at night, why is there a light in the fridge?",
+author:"Me"
+},
+{
+content: "They say ‘don’t try this at home so I’m coming over to your house to try it.",
+author:"Me"
+},
+{
+content: "When you fall, I will be there to catch you with love. Sincerely, the floor.",
+author:"Me"
+},
+{
+content: "I could agree with you, but then we’d both be wrong.",
+author:"Me"
+},
+{
+content: "I didn’t fall, I’m just spending some quality time with the floor.",
+author:"Me"
+},
+{
+content: "Lottery: a tax on people who are bad at math.",
+author:"Me"
+},
+{
+content: "What happens to a frog’s car when it breaks down? It gets toad away.",
+author:"Me"
+},
+{
+content: "I only check my voicemail to get rid of the annoying little icon.",
+author:"Me"
+},
+{
+content: "My windows aren’t dirty, my dog is painting.",
+author:"Me"
+},
+{
+content: "No matter how bad it gets, I’m always rich when I go to the dollar store.",
+author:"Me"
+},
+{
+content: "Today, I laughed until my abs started hurting, so I can skip the gym.",
+author:"Me"
+},
+{
+content: "Brush your teeth",
+author:"Me"
+},
+{
+content: "Wash your hands",
+author:"Me"
+},
+{
+content: "Take 20 minutes",
+author:"Me"
+},
+{
+content: "Lower the brightness of your PC",
+author:"Me"
+},
+{
+content: "Listen music",
+author:"Me"
+},
+{
+content: "Hello there",
+author:"Me"
+},
+{
+content: "Duh, engineering",
+author:"Me"
+},
+{
+content: "Nya",
+author:"Me"
+},
+{
+content: "This is the best place",
+author:"Me"
+},
+{
+content: "Turn up the volume",
+author:"Me"
+},
+{
+content: "Every day is one less day for Christmas",
+author:"Me"
+},
+{
+content: "Synth",
+author:"Me"
+},
+{
+content: "I never finish anyt",
+author:"Me"
+}
+];
+
+//grab html elements
+var quote = document.getElementById('quote-text'),
+    author = document.getElementById('quote-author'),
+    random;
+
+//Generate a Random Quote
+window.onload = randomQuote;
+button.addEventListener('click', randomQuote);
+function randomQuote ()
+{
+//get a random number to pick a random quote object
+random = Math.floor(Math.random() * quoteArray.length);
+//get that random quote's content
+quote.innerHTML = quoteArray[random].content;
+//get that random quote's author
+author.innerHTML = '&mdash; ' + quoteArray[random].author;
+}
